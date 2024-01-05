7c75a7839151c4922078b4959c0525ab3e38d760
pavitpim40_mbp <pavitpim@gmail.com>
2024-01-05 14:53:31 +0700

    auto login

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
 
 const END_POINT = 'https://paybox-wnfo.onrender.com/auth';
 
@@ -8,6 +8,25 @@ export const AuthContext = createContext();
 // S2 : Provider
 export default function AuthContextProvider({ children }) {
   const [user, setUser] = useState(null);
+
+  // fetchMeByToken
+  const fetchUserByToken = () => {
+    fetch(`${END_POINT}/me`, {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('token')}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setUser(data.user);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    fetchUserByToken();
+  }, []);
 
   // login
   const login = (email, password) => {
