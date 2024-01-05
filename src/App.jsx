d605583398d44fd5f8d09c737ac84c919a16a99d
pavitpim40_mbp <pavitpim@gmail.com>
2024-01-05 13:29:16 +0700

    update instruction

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import { useState } from 'react';
 import LoginPage from './components/LoginPage';
 import HomePage from './components/HomePage';
 function App() {
+  // START == AuthContext = Authenticate Context
   const [isLogin, setIsLogin] = useState(false);
+
+  // Challenge :  FN login() แล้วแชร์ให้แทน setIsLogin
+  // Challenge :  FN logout() แล้วแชร์ให้แทน setIsLogin
+  // END == AuthContext = Authenticate Context
 
   return (
     <div className='flex justify-center items-center h-screen'>
-      {!isLogin 
-        ? <LoginPage setIsLogin={setIsLogin} /> 
-        : <HomePage setIsLogin={setIsLogin} />
-      }
+      {!isLogin ? <LoginPage setIsLogin={setIsLogin} /> : <HomePage setIsLogin={setIsLogin} />}
     </div>
   );
 }
